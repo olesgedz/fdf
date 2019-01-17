b968d8f7a6bf3e292456b3db687e837f9d8b72b4
olesgedz <olesgedz@rambler.ru>
2019-01-17 06:51:14 +0300

    d

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@
 NAME = fdf
 
 all:
-	gcc main.c ./minilibx_macos/libmlx.a -framework OpenGL -framework AppKit -o $(NAME)
+	gcc main.c ./minilibx_macos/libmlx.a -framework OpenGL -framework AppKit -o $(NAME) -I ./minilibx_macos/
--- main.c
+++ main.c
@@ -17,6 +17,7 @@ int main(void)
 				        return (EXIT_FAILURE);
 			    if ((data.mlx_win = mlx_new_window(data.mlx_ptr, 640, 480, "Hello world")) == NULL)
 					        return (EXIT_FAILURE);
+					mlx_pixel_put(data.mlx_ptr, data.mlx_win, 10, 10, 0);
 				    mlx_loop(data.mlx_ptr);
 					    return (EXIT_SUCCESS);
 }
