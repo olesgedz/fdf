fdc384e0476564c5944fc6c848a0c8de97641cf8
olesgedz <olesgedz@rambler.ru>
2019-10-15 11:24:52 +0300

    ready to use

--- main.c
+++ main.c
@@ -5,62 +5,65 @@
 
 typedef struct    data_s
 {
-	    void          *mlx_ptr;
-		    void          *win_ptr;
+	void          *mlx_ptr;
+	void          *win_ptr;
+	void  		*img_ptr;
+	char 		*img_data;
+	int width;
+	int height;
+	int bpp;
+	int size_line;
+	int endian;
 }                 data_t;
 
-typedef struct char_s
-{
-	int x;
-	int y;
-	int size;
-} t_char;
+
 int k = 0;
 int j = 0;
 data_t        data;
-int ft_render(int key, void *param)
+
+
+void ft_putpixel(data_t *data,int x, int y, int color)
 {
-	// void new = mlx_new_image(data.mlx_ptr, 500 , 500);
-	// 
-	// mlx_put_image_to_window (data.mlx_ptr, data.win_ptr, 0, 0);
-	if (key == 2)
-		 	 k += 20;
-		if (key == 0)
-			k -= 20;
-		if (key == 13)
-				 j -= 20;
-		if (key == 1)
-				j += 20;
-			printf("x:%d y:%d\n", k, j);
-	int x = 0;
-	int y = 0;
-		//mlx_pixel_put(data.mlx_ptr, data.win_ptr, k, j, 0xFFFFFF);
-	
-	while (  y < 10)
+	((int*)data->img_data)[x + y * data->width] = color;
+
+}
+
+void ft_render(data_t *data)
+{
+	data->img_ptr =  mlx_new_image(data->mlx_ptr, data->width , data->height);
+	data->img_data = mlx_get_data_addr(data->img_ptr, &data->bpp, &data->size_line, &data->endian);
+
+	for (int j = 0; j < 250; j++)
 	{
-		 x = 0;
-		while (x < 10)
+		for (int k = 0; k < 250; k++)
 		{
-			mlx_pixel_put(data.mlx_ptr, data.win_ptr, k+y, j+x, 0xFFFFFF);
-			x++;
+			ft_putpixel(data, k, j, k << 16 | j << 8);
 		}
-		y++;
 	}
-	
+
+
+
+	mlx_put_image_to_window (data->mlx_ptr, data->win_ptr, data->img_ptr, 0, 0);
+}
+
+int ft_press(int key, void *data_void)
+{
+
 	
 	return (0);
 } 
 int main(void)
 {
 	    
+	data.width = 500;
+	data.height = 500;
+	if ((data.mlx_ptr = mlx_init()) == NULL)
+				return (EXIT_FAILURE);
+	if ((data.win_ptr = mlx_new_window(data.mlx_ptr, data.width, data.height, "Hello world")) == NULL)
+				return (EXIT_FAILURE);
 
-		    if ((data.mlx_ptr = mlx_init()) == NULL)
-				        return (EXIT_FAILURE);
-			    if ((data.win_ptr = mlx_new_window(data.mlx_ptr, 500, 500, "Hello world")) == NULL)
-					        return (EXIT_FAILURE);
-					
-						printf("%d", mlx_key_hook(data.win_ptr, ft_render, (void *)0));
-					//	mlx_string_put(data.mlx_ptr, data.win_ptr, j, k, 0xFFFFFF, "HELLO");
-				    mlx_loop(data.mlx_ptr);
-					    return (EXIT_SUCCESS);
+	ft_render(&data);
+	printf("%d", mlx_key_hook(data.win_ptr, ft_press, &data));
+	mlx_loop(data.mlx_ptr);
+	return (EXIT_SUCCESS);
 }
